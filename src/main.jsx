46e17a7d6ca00d9e8b80b38ff04bdeb7e6256517
rs123489 <rprp5804@gmail.com>
2025-07-15 12:03:03 +0530

    k

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {store} from './redux/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <Provider store={store }>
     <App />
   </Provider>,
 )
